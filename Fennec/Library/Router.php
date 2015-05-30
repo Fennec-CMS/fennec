1998b6cbd249400e5b78bab3c6fbe866e18da553
David Lima <dave@blogdodave.com.br>
2015-05-30 17:57:29 -0300

    Turning 'params' optional on Router

--- Fennec/Library/Router.php
+++ Fennec/Library/Router.php
@@ -23,10 +23,12 @@ class Router
         $url = $_SERVER['REQUEST_URI'];
         foreach (self::$routes as $route) {
             if (preg_match($route['route'], $url, $matches)) {
-                
-                array_shift($matches);
-                self::$params = array_combine($route['params'], $matches);
-                
+
+                if (isset($route['params']) && $route['params']) {
+                    array_shift($matches);
+                    self::$params = array_combine($route['params'], $matches);
+                }
+
                 self::$matchingRoute = $route;
                 
                 self::$view = $route['controller'] . "/" . $route['action'];
