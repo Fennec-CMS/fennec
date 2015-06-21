f4233ca8cd580a6093fa1009fa62a485cdf37f52
David Lima <dave@blogdodave.com.br>
2015-06-20 23:14:46 -0300

    Enabling user name and register date on top menu

--- Fennec/Model/Administrators.php
+++ Fennec/Model/Administrators.php
@@ -105,6 +105,7 @@ class Administrators extends Base
 
         if ($userData) {
             if ($this->verify($this->password, $userData->getPassword())) {
+                $userData->setSince(new \DateTime($userData->getSince()));
                 $_SESSION['fennecAdmin'] = $userData;
                 return true;
             }
