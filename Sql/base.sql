1f2f76b3c73779ea52fe8f74a8c3d324600a1a95
David Lima <dave@blogdodave.com.br>
2015-08-31 20:40:09 -0300

    Including base SQL file

---
+++ Sql/base.sql
@@ -0,0 +1,22 @@
+CREATE TABLE administrators (
+  name VARCHAR(32) NOT NULL,
+  password VARCHAR(255) NOT NULL,
+  profile INT,
+  username VARCHAR(32),
+  email VARCHAR(255),
+  since DATE,
+  id SERIAL
+);
+
+/*
+Default password is: 123456
+To generate a different password, please run:
+
+php -r "echo password_hash('YOUR_PASSWORD_HERE', PASSWORD_DEFAULT) . PHP_EOL;";
+
+And paste the output before run the query below.
+You still can change your password after CMS setup.
+*/
+
+INSERT INTO administrators (name, password, profile, username) VALUES
+('Administrator', '$2y$10$09z91PkP4SCxD0CceWEe6egjlDZzYpoMpF4R19CSsYmDMYCtnF8MS', 1, 'admin');
