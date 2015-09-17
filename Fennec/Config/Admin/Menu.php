53a124fe9f74e0a466fd3278a9428d80e95bd0e9
David Lima <dave@blogdodave.com.br>
2015-09-16 21:43:31 -0300

    Including default menu icons on base routes (Dashboard and Administrators)

--- Fennec/Config/Admin/Menu.php
+++ Fennec/Config/Admin/Menu.php
@@ -2,9 +2,11 @@
 return array(
     'Dashboard' => array(
         'route' => 'admin',
+        'icon' => 'fa fa-dashboard'
     ),
     'Administrators' => array(
         'route' => 'admin-administrators-list',
+        'icon' => 'fa fa-user',
         'subitems' => array(
             'Create new' => 'admin-administrators-add',
             'List' => 'admin-administrators-list'
